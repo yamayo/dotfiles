640ef21ec992d43d49b8fd96b1d3f9d3e363d4e0
yamayo <noorthaven@gmail.com>
2022-10-19 00:10:47 +0900

    update

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -13,14 +13,3 @@ keymap.set('i', '<C-j>', '<Down>')
 keymap.set('i', '<C-k>', '<Up>')
 keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
-
-
--- COC
--- local opt = { expr = true, silent = true, replace_keycodes = false }
--- local opts = { silent = false, noremap = true, expr = true, replace_keycodes = false }
--- keymap.set('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<CR>"]], opts)
--- keymap.set("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
--- -- keymap.set("i", "<CR>", 'coc#pum#visible() ? coc#pum#confirm() : "<CR>"', opts)
--- keymap.set('i', '<Tab>', 'coc#pum#visible() ? coc#pum#confirm() : "<Tab>"', opt)
--- keymap.set('i', '<Tab>', 'coc#pum#visible() ? coc#pum#next(1) : "<Tab>"', opt)
--- keymap.set('i', '<S-Tab>', 'coc#pum#visible() ? coc#pum#prev(1) : "<S-Tab>"', opts)

ec8e9f7c4ad453fe6c2ee879e15fef5784b25e71
yamayo <noorthaven@gmail.com>
2023-02-18 00:51:14 +0900

    update

---
+++ changelog.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  disableEmoji: true,
+  enableWritingScopes: true,
+  scopes: ["front", "api", "infra", ""],
+  questions: [
+    "type",
+    "scope",
+    "subject",
+    // "body",
+    // "breaking",
+    // "issues",
+    // "lerna",
+  ],
+};
